b4a706283fe65a0bd1c1783faf61c2fab8e32e7b
Daichi TOMA <amothic@gmail.com>
2011-11-26 19:20:43 +0900

    regular execution

--- chat/ViewController.h
+++ chat/ViewController.h
@@ -8,6 +8,31 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UITableViewDataSource,UITableViewDelegate>
+{
+    IBOutlet UITextField *messageText;
+    IBOutlet UIButton *sendButton;
+    IBOutlet UITableView *messageList;
+    
+    NSMutableData *receivedData;
+    NSMutableArray *messages;
+    int lastId;
+    
+    NSTimer *timer;
+    
+    NSXMLParser *chatParser;
+    NSString *msgAdded;
+    NSMutableString *msgUser;
+    NSMutableString *msgText;
+    int msgId;
+    Boolean inText;
+    Boolean inUser;
+}
+
+@property (nonatomic,retain) UITextField *messageText;
+@property (nonatomic,retain) UIButton *sendButton;
+@property (nonatomic,retain) UITableView *messageList;
+
+- (IBAction)sendClicked:(id)sender;
 
 @end
